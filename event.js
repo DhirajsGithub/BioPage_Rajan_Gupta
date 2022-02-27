3dd3301653f7e03dada0add375a642bdda50efd4
Dhiraj Borse <borsedhiraj123@gmail.com>
2022-02-27 21:13:20 +0530

    all files of BioPage added

---
+++ event.js
@@ -0,0 +1,115 @@
+const link1 = document.querySelector(".tooltip-1")
+const link2 = document.querySelector(".tooltip-2")
+const link3 = document.querySelector(".tooltip-3")
+const link4 = document.querySelector(".tooltip-4")
+const link5 = document.querySelector(".tooltip-5")
+
+
+const popout1 = document.querySelector(".pop-outin-1")
+const popout2 = document.querySelector(".pop-outin-2")
+const popout3 = document.querySelector(".pop-outin-3")
+const popout4 = document.querySelector(".pop-outin-4")
+
+
+const mainHd = document.querySelector(".main-hd")
+
+const resize = document.querySelector(".para .resize")
+
+// let name = "Rajan Gupta"
+
+// mainHd.style.fontSize = "5rem"
+// mainHd.textContent = ""
+// for (let char of "Rajan"){
+//     mainHd.style.textContent += char
+    
+// }
+// window.addEventListener("load", ()=>{
+//     mainHd.style.opacity = "1"
+// })
+
+// setInterval(()=>{
+//     mainHd.style.opacity = "1"
+//     mainHd.textContent = "R"
+// },2000)
+
+const ani1 = document.querySelector(".ani1")
+const ani2 = document.querySelector(".ani2")
+const ani3 = document.querySelector(".ani3")
+const ani4 = document.querySelector(".ani4")
+const ani5 = document.querySelector(".ani5")
+
+
+ani1.style.transition = "transform 1s ease-in-out"
+link1.style.transition = "opacity 1s ease-in-out"
+ani2.style.transition = "transform 1.4s ease-in-out"
+link2.style.transition = "opacity 1.5s ease-in-out"
+ani3.style.transition = "transform 1.8s ease-in-out"
+link3.style.transition = "opacity 2s ease-in-out"
+ani4.style.transition = "transform 2.2s ease-in-out"
+link4.style.transition = "opacity 2.5s ease-in-out"
+ani5.style.transition = "transform 2.6s ease-in-out"
+link5.style.transition = "opacity 3s ease-in-out"
+// resize.style.transition = "opacity 3s ease-in-out"
+mainHd.style.transition = "opacity 1.5s ease-in-out"
+
+// resize.style.fontSize = "2.6rem"
+
+
+// mainHd.style.transition = "all 1s ease-in-out"
+
+
+window.addEventListener("load", ()=>{
+    link1.style.opacity = "1"
+    ani1.style.transform = "translateY(-40px)"
+    link2.style.opacity = "1"
+    ani2.style.transform = "translateY(-40px)"
+    link3.style.opacity = "1"
+    ani3.style.transform = "translateY(-40px)"
+    link4.style.opacity = "1"
+    ani4.style.transform = "translateY(-40px)"
+    link5.style.opacity = "1"
+    ani5.style.transform = "translateY(-40px)"
+
+    mainHd.style.opacity = "1"
+
+    // resize.style.opacity = "5"
+
+    // mainHd.textConten = ""
+    // for(let char of "rajan"){
+    //     mainHd.textContent += char
+    // }
+
+
+})
+
+
+
+
+
+// window.addEventListener("load", () => {
+//     // links.style.display = "inlineBlock"
+//     link1.style.transform = "translateY(-40px)"
+// });
+
+
+// function show (){
+//     link1.style.background = "red";
+// }
+
+
+// links.style.display = "inlie-block"
+// for (link of links){
+//     // setInterval(()=>{
+//     //     link.style.display = "inline-block"
+//     // },1000)
+   
+// }
+// window.addEventListener("load", () => {
+//     links.style.display = "inlineBlock"
+//     // link1.style.transform = "translateY(-40px)";
+// });
+
+// function hover(){
+//     link1.style.
+// }
+// window.CSSAnimation.
